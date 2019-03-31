b53118a96e790713a73d4bec078bd13b6e485d85
freewind <nowindlee@gmail.com>
2019-03-31 18:50:17 +0800

    demo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-TypeScript React Select Demo
+TypeScript React Select Loading Demo
 =================================
 
-既可以输入又可以选择的Select组件。
+如何想让从远程获取数据时，select组件显示`loading...`的信息，需要配合`isLoading`和`loadingMessage`
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,5 +1,5 @@
 //import * as React from 'react'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Select from 'react-select';
 import {ValueType, OptionsType} from "react-select/lib/types";
 
@@ -8,17 +8,30 @@ type OptionType = {
   label: string
 }
 
-const options: OptionsType<OptionType> = [
+const loadedOptions: OptionsType<OptionType> = [
   {value: 'chocolate', label: 'Chocolate'},
   {value: 'strawberry', label: 'Strawberry'},
   {value: 'vanilla', label: 'Vanilla'}
 ];
 
 export default function Hello() {
+  const [options, setOptions] = useState<OptionsType<OptionType>>([])
   const [selectedOption, setSelectedOption] = useState<ValueType<OptionType>>(null)
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setOptions(loadedOptions)
+      setLoading(false)
+    }, 3000)
+  }, []);
+
   return <div>
     <h1>Hello React Select</h1>
-    <Select value={selectedOption} onChange={setSelectedOption} options={options}/>
+    <Select value={selectedOption} onChange={setSelectedOption} options={options}
+            isLoading={isLoading}
+            loadingMessage={() => 'loading ...'}/>
     <div>{JSON.stringify(selectedOption)}</div>
   </div>
 };
